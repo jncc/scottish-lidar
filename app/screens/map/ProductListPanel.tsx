e7af94ca11c68b09f8e5020ae223d1a9b1712ea4
Pete Montgomery <sumo.the.cat@gmail.com>
2019-10-29 21:34:33 +0000

    Improved product list

--- app/screens/map/ProductListPanel.tsx
+++ app/screens/map/ProductListPanel.tsx
@@ -23,52 +23,58 @@ export const ProductListPanel = (props: Props) => {
   if (props.collection) {
     return (
       <div className="product-list-panel">
-        <h5>
-          <i className="fas fa-th text-primary mr-2" />
-          {props.collection.collection.metadata.title}
-        </h5>
-        <div className="mb-2">
-          <DatasetPath dataset={props.collection.path.dataset} />
-        </div>
-        
-        <div className="product-list-panel-abstract">
-          <i className="fas fa-info-circle text-secondary mr-2" />
-          {props.collection.collection.metadata.abstract}
-        </div>
-
-        {props.products.result.length === 0 &&
-        <div>
-          <div>
-            No products in this dataset matched your query. 
+        <div className="">
+          <h5>
+            <i className="fas fa-th text-primary mr-2" />
+            {props.collection.collection.metadata.title}
+          </h5>
+          <div className="mb-2">
+            <DatasetPath dataset={props.collection.path.dataset} />
           </div>
-          <div>
-            Perhaps select a different dataset.
+          
+          <div className="product-list-panel-abstract">
+            <i className="fas fa-info-circle text-secondary mr-2" />
+            {props.collection.collection.metadata.abstract}
           </div>
-        </div>
-        }
 
-        <hr />
-        {props.products.result.map(p => <ProductListItem
-          key={p.id}
-          product={p}
-          hovered={props.hoveredProduct === p}
-          productHovered={props.productHovered}
-          productUnhovered={props.productUnhovered}
-        />)}
-        <hr />
-        {props.productCountForCurrentCollection &&
-        <div>
-          Showing {props.products.result.length} of {props.productCountForCurrentCollection} matching products
-          {/* Actually want: Showing 11 to 20 of 36 matching products */}
-          <br />
-          <br />
-          <Pager
-            offset={props.products.query.offset || 0}
-            total={props.productCountForCurrentCollection}
-            setPage={props.setPage}
-          />
+          {props.products.result.length === 0 &&
+          <div>
+            <div>
+              No products in this dataset matched your query. 
+            </div>
+            <div>
+              Perhaps select a different dataset.
+            </div>
+          </div>
+          }
         </div>
-        }
+        <div className="product-list-items">
+          <hr />
+          {props.products.result.map(p => <ProductListItem
+            key={p.id}
+            product={p}
+            hovered={props.hoveredProduct === p}
+            productHovered={props.productHovered}
+            productUnhovered={props.productUnhovered}
+          />)}
+
+        </div>
+        <div>
+          <hr />
+          {props.productCountForCurrentCollection &&
+          <div>
+            Showing {props.products.result.length} of {props.productCountForCurrentCollection} matching products
+            {/* Actually want: Showing 11 to 20 of 36 matching products */}
+            <br />
+            <br />
+            <Pager
+              offset={props.products.query.offset || 0}
+              total={props.productCountForCurrentCollection}
+              setPage={props.setPage}
+            />
+          </div>
+          }
+        </div>
       </div>
     )
   }
