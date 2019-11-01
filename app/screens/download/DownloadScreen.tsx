16255eaa14a650e91b5558f966f06f00a64a7b6b
Pete Montgomery <sumo.the.cat@gmail.com>
2019-11-01 16:57:47 +0000

    Introduce framer motion

--- app/screens/download/DownloadScreen.tsx
+++ app/screens/download/DownloadScreen.tsx
@@ -2,11 +2,20 @@
 import * as React from 'react'
 import { useBasket } from '../../basket'
 
+import { motion } from 'framer-motion'
+import { Button } from 'react-bootstrap'
+
 type Props = {
 } 
 
+// const variants = {
+//   hidden: { opacity: 0 },
+//   visible: { opacity: 1 },
+// }
+
 export const DownloadScreen = (props: Props) => {
   
+  let [visible, setVisible] = React.useState(true)
   let [basket] = useBasket()
 
   let basketItemElements = basket.items.map(item =>
@@ -17,6 +26,19 @@ export const DownloadScreen = (props: Props) => {
 
   return (
     <div>
+      <div>
+        <Button onClick={() => setVisible((prev => !prev))}>
+          Click me
+        </Button>
+      </div>
+      <motion.div
+        // initial="hidden"
+        animate={{ opacity: visible ? 1 : 0}}
+        // variants={variants}
+      >
+        Hello
+      </motion.div>
+
       <h1>Download</h1>
       <ul>
         {basketItemElements}
