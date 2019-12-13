1f1c9b8860c6df5dc8be60529265c4c8821484c0
Pete Montgomery <sumo.the.cat@gmail.com>
2019-12-13 12:15:01 +0000

    Temp remove types for react-bootstrap

--- app/declarations.d.ts
+++ app/declarations.d.ts
@@ -5,6 +5,10 @@ declare module '*.png'
 declare module 'react-copy-to-clipboard'
 declare module 'react-router-dom'
 
+// the latest react-bootstrap release causes typescript errors
+// ignore for now but remove this when fixed
+declare module 'react-bootstrap'
+
 // declare our own types for leaflet-editable plugin
 // @types/leaflet-editable currently causes compile errors
 declare namespace L {
