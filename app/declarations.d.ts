95292f38308136040c9aa182fabfcdfb90ccdb5b
Pete Montgomery <sumo.the.cat@gmail.com>
2019-12-13 14:24:50 +0000

    Set skipLibCheck to workaround react-bootstrap type errors

--- app/declarations.d.ts
+++ app/declarations.d.ts
@@ -5,10 +5,6 @@ declare module '*.png'
 declare module 'react-copy-to-clipboard'
 declare module 'react-router-dom'
 
-// the latest react-bootstrap release causes typescript errors
-// ignore for now but remove this when fixed
-declare module 'react-bootstrap'
-
 // declare our own types for leaflet-editable plugin
 // @types/leaflet-editable currently causes compile errors
 declare namespace L {
