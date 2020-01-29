867a5103de471dd3f70eb98ee11d0f1c932b423f
Pete Montgomery <sumo.the.cat@gmail.com>
2020-01-29 14:39:30 +0000

    Correct type error and check required env vars

--- env.ts
+++ env.ts
@@ -3,13 +3,35 @@
 // and makes them available via the process.env object.
 // See https://parceljs.org/env.html
 
+// slightly long-winded way of accessing process.env variables due to
+// https://github.com/parcel-bundler/parcel/issues/2191 and related
+
+let NODE_ENV                 = process.env.NODE_ENV as 'development' | 'production'
+let CATALOG_API_ENDPOINT     = process.env.CATALOG_API_ENDPOINT
+let AGGREGATE_LAYER_BASE_URL = process.env.AGGREGATE_LAYER_BASE_URL
+let PAGE_EXTENSION           = process.env.PAGE_EXTENSION
+let INDEX_PAGE               = process.env.INDEX_PAGE
+
+if (!CATALOG_API_ENDPOINT) {
+  throw(`Environment variable CATALOG_API_ENDPOINT is required.`)
+}
+if (!AGGREGATE_LAYER_BASE_URL) {
+  throw(`Environment variable AGGREGATE_LAYER_BASE_URL is required.`)
+}
+if (!PAGE_EXTENSION) {
+  throw(`Environment variable PAGE_EXTENSION is required.`)
+}
+if (!INDEX_PAGE) {
+  throw(`Environment variable INDEX_PAGE is required.`)
+}
+
 /**
  * Provides the environment variables for the application.
  */
 export const env = {
-    NODE_ENV                : process.env.NODE_ENV as 'development' | 'production',
-    CATALOG_API_ENDPOINT    : process.env.CATALOG_API_ENDPOINT,
-    AGGREGATE_LAYER_BASE_URL: process.env.AGGREGATE_LAYER_BASE_URL,
-    PAGE_EXTENSION          : process.env.PAGE_EXTENSION,
-    INDEX_PAGE              : process.env.INDEX_PAGE,
+    NODE_ENV,
+    CATALOG_API_ENDPOINT,
+    AGGREGATE_LAYER_BASE_URL,
+    PAGE_EXTENSION,
+    INDEX_PAGE,
 }
