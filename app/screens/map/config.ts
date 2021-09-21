295dd3b05dec68a19bf781bc2cfc698a456fe6ea
Pete Montgomery <sumo.the.cat@gmail.com>
2021-09-21 12:53:46 +0100

    Use viaeuropa base map

--- app/screens/map/config.ts
+++ app/screens/map/config.ts
@@ -7,7 +7,9 @@ export const config = {
   defaultZoom: 8,
   maximumZoom: 13,
   defaultCenter: [56.4, -4] as [number, number],
-  baseLayerUrlTemplate: `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoiam5jYy13ZWJtYXN0ZXIiLCJhIjoiY2s5c2wyeXdxMG8weTNlcWt6dmw3Nno4cCJ9.zSDqXxiNyofO3Ilq-hyfqg`,
+  // baseLayerUrlTemplate: `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoiam5jYy13ZWJtYXN0ZXIiLCJhIjoiY2s5c2wyeXdxMG8weTNlcWt6dmw3Nno4cCJ9.zSDqXxiNyofO3Ilq-hyfqg`,
+
+  baseLayerUrlTemplate: `https://tile.viaeuropa.uk.com/osmao-scotg-ov012-a8f54/m0335/{z}/{x}/{y}.png`,
   attribution: `Backdrop &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>`,
   aggregateLayer: {
     baseUrl: env.AGGREGATE_LAYER_BASE_URL,
