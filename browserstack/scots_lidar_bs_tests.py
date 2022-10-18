82d45c09111db235620bc7fa03e13d83fb42997b
martin bamford <martin.bamford@jncc.gov.uk>
2022-10-18 16:21:46 +0100

    Add test case for registering a selected 10km grid square

--- browserstack/scots_lidar_bs_tests.py
+++ browserstack/scots_lidar_bs_tests.py
@@ -10,6 +10,7 @@ from selenium.webdriver.safari.options import Options as SafariOptions
 from selenium.webdriver.edge.options import Options as EdgeOptions
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from threading import Thread
@@ -22,13 +23,26 @@ BROWSERSTACK_URL = config('BROWSERSTACK_URL')
 
 BUILD_NAME = "Scots LiDAR automated testing"
 URL_UNDER_TEST = "https://remotesensingdata.gov.scot/"
-EXPECTED_MAIN_PAGE_TITLE = "Scottish Remote Sensing Portal | Scottish Government"
-EXPECTED_DATASETS_PAGE_TITLE = "Scottish LiDAR Remote Sensing datasets | Scottish Government"
+MAIN_PAGE_TITLE = "Scottish Remote Sensing Portal | Scottish Government"
+DETAILS_PAGE_TITLE = "Scottish LiDAR Remote Sensing datasets | Scottish Government"
 ACCEPT_COOKIES_BUTTON_XPATH = '//*[@id="ccc-notify-accept"]'
-BROWSE_DATASETS_BUTTON_XPATH = '//*[@id="main"]/div/div[1]/a'
+BROWSE_DATASETS_BUTTON_XPATH = '//*[@id="main"]/div/div[1]/a[1]'
+EXPLORE_MAP_BUTTON_XPATH = '//*[@id="main"]/div/div[1]/a[2]'
 DATASETS_HEADER_SHOWING_X_OF_Y_XPATH = '//*[@id="app"]/div[1]/div[1]/div[1]/div[1]/span'
 DATASETS_LIST_XPATH = '//*[@id="app"]/div[1]/div[2]'
 DATASETS_LIST_CLASS = 'list-item'
+DATASETS_PAGE_LOAD_WAIT_TIME_SECONDS = 2.5
+MAP_DATASET_LIST_LIDAR_PHASE2_DTM_LABEL_XPATH = '//*[@id="main"]/div[1]/div[2]/div[1]/div[1]/div[2]/div[3]/div[2]/div[2]/div[1]/div'
+MAP_10KM_TILES_COLLECTION_XPATH = '//*[@id="main"]/div[1]/div[2]/div[3]/div[1]/div[3]'
+MAP_LIDAR_ITEM_LIST_XPATH = '//*[@id="main"]/div[1]/div[2]/div[1]/div[2]/div[2]/div[2]'
+MAP_LIDAR_BASKET_COUNT_XPATH = '//*[@id="main"]/div[1]/div[2]/div[1]/div[2]/div[3]/div[4]/span'
+MAP_DATASET_ITEM_CLASS = 'dataset-list-item'
+MAP_TILES_CLASS = 'leaflet-interactive'
+MAP_LIDAR_PHASE2_DTM_NN55_SQUARE_TILE_INDEX = 2
+MAP_LIDAR_PHASE2_DTM_NN55_SQUARE_LIST_INDEX = 1
+MAP_PAGE_LOAD_WAIT_TIME_SECONDS = 10 # NB pseudo-random test failures observed if wait time lowered to 5 seconds
+LIDAR_BASKET_ITEM_CLASS = 'product-list-item-basket'
+LIDAR_SELECTED_BASKET_ITEM_CLASS = 'product-list-item-basket-in'
 
 def get_browser_option(browser):
     switcher = {
@@ -55,13 +69,14 @@ def wait_page(driver, title):
 def wait_xpath_elem(driver, path):	
     return WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, path)))
     
+def perform_svg_element_compatible_click(driver, element):
+    driver.execute_script("arguments[0].dispatchEvent(new MouseEvent('click', {view: window, bubbles:true, cancelable: true}))", element)
+
 def test_multiple_datasets_available(driver) :
-   driver.get(URL_UNDER_TEST)
-   wait_page(driver, EXPECTED_MAIN_PAGE_TITLE)
    wait_xpath_elem(driver, ACCEPT_COOKIES_BUTTON_XPATH).click() 
    wait_xpath_elem(driver, BROWSE_DATASETS_BUTTON_XPATH).click()
-   wait_page(driver, EXPECTED_DATASETS_PAGE_TITLE)
-   time.sleep(2.5)
+   wait_page(driver, DETAILS_PAGE_TITLE)
+   time.sleep(DATASETS_PAGE_LOAD_WAIT_TIME_SECONDS)
    datasets_header = driver.find_element(By.XPATH, DATASETS_HEADER_SHOWING_X_OF_Y_XPATH).text
    showing_x_of_y = [int(s) for s in re.findall(r'\d+', datasets_header)]
    if len(showing_x_of_y) != 2:
@@ -77,14 +92,38 @@ def test_multiple_datasets_available(driver) :
            list_elems = list_div.find_elements(By.CLASS_NAME, DATASETS_LIST_CLASS)
            dataset_count = len(list(list_elems))
            if dataset_count != showing_total_datasets:
-                  fail_test(driver, "Number of datasets in list does not match total value in header")
+               fail_test(driver, "Number of datasets in list does not match total value in header")
            else: 
-               pass_test(driver, "Multiple datasets available!")        
+               pass_test(driver, "Multiple datasets available!")
+
+def test_selected_10km_grid_square_registered(driver) :
+   wait_xpath_elem(driver, ACCEPT_COOKIES_BUTTON_XPATH).click() 
+   wait_xpath_elem(driver, EXPLORE_MAP_BUTTON_XPATH).click()
+   wait_page(driver, DETAILS_PAGE_TITLE)
+   wait_xpath_elem(driver, MAP_DATASET_LIST_LIDAR_PHASE2_DTM_LABEL_XPATH).click()
+   time.sleep(MAP_PAGE_LOAD_WAIT_TIME_SECONDS)
+   tiles_10km_collection = driver.find_element(By.XPATH, MAP_10KM_TILES_COLLECTION_XPATH)
+   tile_nn55 = tiles_10km_collection.find_elements(By.CLASS_NAME, MAP_TILES_CLASS)[MAP_LIDAR_PHASE2_DTM_NN55_SQUARE_TILE_INDEX]
+   perform_svg_element_compatible_click(driver, tile_nn55)
+   lidar_basket_count = driver.find_element(By.XPATH, MAP_LIDAR_BASKET_COUNT_XPATH).text
+   if '1' not in lidar_basket_count:
+       fail_test(driver, "Expected LiDAR basket to contain 1 item after clicking on NN55 10km grid square")
+   else: 
+       lidar_items = driver.find_element(By.XPATH, MAP_LIDAR_ITEM_LIST_XPATH)
+       lidar_baskets = lidar_items.find_elements(By.CLASS_NAME, LIDAR_BASKET_ITEM_CLASS)
+       if LIDAR_SELECTED_BASKET_ITEM_CLASS not in lidar_baskets[MAP_LIDAR_PHASE2_DTM_NN55_SQUARE_LIST_INDEX].get_attribute("class"):
+           fail_test(driver, "LiDAR basket icon corresponding to NN55 10km grid square should be highlighted after clicking on the grid square") 
+       else:
+           pass_test(driver, "Selected grid square for LiDAR Phase 2 DTM NN55 successfully registered!")             
 
 tests = [
     {
         "name": "Test Multiple Datasets Available",
         "function": test_multiple_datasets_available,
+    },
+    {
+        "name": "Test Selected 10km Grid Square is registered",
+        "function": test_selected_10km_grid_square_registered,
     },
 ]
 
@@ -103,11 +142,14 @@ def run_session(cap):
             bstack_options["sessionName"] = test["name"]
             options.set_capability('bstack:options', bstack_options)
             driver = webdriver.Remote(command_executor=BROWSERSTACK_URL,options=options)
+            driver.maximize_window()  # The Scots LiDAR website will not load maps in Selenium's smaller default window size
+            driver.get(URL_UNDER_TEST)
+            wait_page(driver, MAIN_PAGE_TITLE)
             test["function"](driver)
         except NoSuchElementException:
             fail_test(driver, "Some elements failed to load")
         except Exception:
-            fail_test(driver, "Some exception occurred")
+            fail_test(driver, "Some exception occurred")    
         driver.quit()
     
 for cap in capabilities:
